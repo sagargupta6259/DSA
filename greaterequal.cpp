227737b360bdb54a9eaba6f9351f13b8432fbe36
sagar kumar gupta <sg6259151@gmail.com>
2026-06-10 13:16:38 +0530

    Update greaterequal.cpp

--- greaterequal.cpp
+++ greaterequal.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
-
     int arr[n];
-
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     int ans[n];
-
     for(int i = 0; i < n; i++) {
         ans[i] = -1;
-
         for(int j = i + 1; j < n; j++) {
             if(arr[j] >= arr[i]) {
                 ans[i] = arr[j];
@@ -23,10 +17,8 @@ int main() {
             }
         }
     }
-
     for(int i = 0; i < n; i++) {
         cout << ans[i] << " ";
     }
-
     return 0;
 }
